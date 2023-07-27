b107ef9f8fd9e9f7c893fa897f49fce81ee0910d
harsit09 <84085808+harsit09@users.noreply.github.com>
2023-07-28 00:18:55 +0530

    Create simplerDATAType.js

---
+++ simplerDATAType.js
@@ -0,0 +1,23 @@
+74ea564f92aed76cea91589b74b0e5028f2f20f8 //sha
+
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
